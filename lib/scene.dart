3eea46e7897315239a7c095e370e11e83d994884
Ivan Yuriev <ivanyr44@gmail.com>
2022-12-19 10:42:57 +0300

    scene classes rework

--- lib/scene.dart
+++ lib/scene.dart
@@ -1,27 +1,62 @@
-import 'package:visual_novel/condition.dart';
+import 'package:visual_novel/route.dart';
 
-class Scene {
-  final String id;
-  final String? text;
-  final Map<String, int>? characters;
-  final String? background;
-  final String? music;
+abstract class Scene {
+  final String _id;
+  Scene(this._id);
 
-  ///Игра должна проверять каждое условие
-  ///и переходить на сцену с айди под выполненным условием
-  final List<Route>? routes;
+  String get id => _id;
+}
 
-  ///Если никаких условий не задано, должен быть задан адрес
-  ///следующей сцены для безусловного перехода
-  final String nextScene;
+abstract class GenericScene extends Scene {
+  final String? _text;
+  final String? _music;
+  final Map<String, int>? _characters;
+  GenericScene(super.id, this._text, this._music, this._characters);
 
-  ///Проходимся по списку условий и находим айди следующей сцены
-  String getNextSceneId() {
-    for (var route in routes ?? []) {
-      if (route.checkCondition?.call() ?? false) {
-        return route.nextScene;
-      }
-    }
-    return nextScene;
-  }
+  String? get text => _text;
+  String? get music => _music;
+  Map<String, int>? get characters => _characters;
+}
+
+///Сцена без выборов
+class SimpleScene extends GenericScene {
+  final Scene _nextScene;
+
+  SimpleScene(
+      {required String id,
+      String? text,
+      String? music,
+      Map<String, int>? characters,
+      required Scene nextScene})
+      : _nextScene = nextScene,
+        super(id, text, music, characters);
+
+  Scene get nextScene => _nextScene;
+}
+
+///Сцена с несколькими (>0) выборами
+class MultipleChoiceScene extends GenericScene {
+  ///Список с айди выборов из binding
+  final List<String> _choices;
+
+  MultipleChoiceScene(
+      {required String id,
+      String? text,
+      String? music,
+      Map<String, int>? characters,
+      required List<String> choices})
+      : _choices = choices,
+        super(id, text, music, characters);
+
+  List<String> get choices => _choices;
+
+  //TODO: обработка выбора с заданным айди
+}
+
+///Сцена с минимумом требований, внутри которой может быть что угодно,
+///даже игра в очко
+class InteractiveScene extends Scene {
+  InteractiveScene(super.id);
+
+  //TODO: возможность отображать любой виджет(?) внутри такой сцены
 }
