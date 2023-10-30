76ffbc429ab18d4d929ef0b406788308955a9cad
peaashmeter <ivanyr44@gmail.com>
2023-10-30 20:41:51 +0300

    clearing things up

--- lib/core/scene.dart
+++ lib/core/scene.dart
@@ -5,16 +5,8 @@ class Scene {
   final Function(Stage stage, NovelState state)? script;
   final String? description;
 
-  ///A timestamp to differentiate two instances of a same scene, loaded in different times
-  final int? token;
-
   Scene({
     required this.script,
     required this.description,
-  }) : token = DateTime.timestamp().millisecondsSinceEpoch;
-
-  ///Returns a copy of [other] with a new token
-  factory Scene.from(Scene other) {
-    return Scene(script: other.script, description: other.description);
-  }
+  });
 }
