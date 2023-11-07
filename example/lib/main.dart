14c2ab9d2980379f6849c487f9ac115856715b6a
peaashmeter <ivanyr44@gmail.com>
2023-11-07 23:04:27 +0300

    theme refactoring

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,50 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Npdart Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
-        fontFamily: 'Marmelad',
-        textTheme: TextTheme(
-          //Style for choices
-          headlineMedium: TextStyle(
-              color: Colors.yellow.shade100,
-              fontSize: 24,
-              shadows: const [
-                Shadow(
-                  blurRadius: 2,
-                  offset: Offset(2, 2),
-                ),
-              ]),
-          //style for text input
-          titleMedium:
-              const TextStyle(color: Colors.white, fontSize: 16, shadows: [
-            Shadow(
-              blurRadius: 2,
-              offset: Offset(2, 2),
-            ),
-          ]),
-          //style for headers
-          headlineSmall: const TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              shadows: [
-                Shadow(
-                  blurRadius: 2,
-                  offset: Offset(2, 2),
-                ),
-              ]),
-          //style for dialog strings
-          titleLarge: TextStyle(color: Colors.yellow.shade100, shadows: const [
-            Shadow(
-              blurRadius: 2,
-              offset: Offset(2, 2),
-            ),
-          ]),
-        ),
-      ),
+      title: 'npdart demo',
       home: Novel(
         initialState: saveData,
         tree: Tree(scenes: scenes),
@@ -74,3 +31,44 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+  fontFamily: 'Marmelad',
+  textTheme: TextTheme(
+    //Style for choices
+    headlineMedium:
+        TextStyle(color: Colors.yellow.shade100, fontSize: 24, shadows: const [
+      Shadow(
+        blurRadius: 2,
+        offset: Offset(2, 2),
+      ),
+    ]),
+    //style for text input
+    titleMedium: const TextStyle(color: Colors.white, fontSize: 16, shadows: [
+      Shadow(
+        blurRadius: 2,
+        offset: Offset(2, 2),
+      ),
+    ]),
+    //style for headers
+    headlineSmall: const TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        shadows: [
+          Shadow(
+            blurRadius: 2,
+            offset: Offset(2, 2),
+          ),
+        ]),
+    //style for dialog strings
+    titleLarge: TextStyle(color: Colors.yellow.shade100, shadows: const [
+      Shadow(
+        blurRadius: 2,
+        offset: Offset(2, 2),
+      ),
+    ]),
+  ),
+);
